ff02858722c6034d3c738a35f4015e6337cbd8c3
Rpwnage <orksec@protonmail.com>
2021-02-18 13:01:42 +0100

    rootfs files

---
+++ reton/Jailbreak/rootfs.h
@@ -0,0 +1,12 @@
+//
+//  rootfs.h
+//  reton
+//
+//  Created by Luca on 18.02.21.
+//
+
+#ifndef rootfs_h
+#define rootfs_h
+
+
+#endif /* rootfs_h */
---
+++ reton/Jailbreak/rootfs.m
@@ -0,0 +1,8 @@
+//
+//  rootfs.m
+//  reton
+//
+//  Created by Luca on 18.02.21.
+//
+
+#import <Foundation/Foundation.h>
