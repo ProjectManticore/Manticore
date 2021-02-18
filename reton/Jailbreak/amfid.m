47f80c5b440e0086b3fea4987466906c78f05d16
Rpwnage <orksec@protonmail.com>
2021-02-18 13:42:25 +0100

    IOKit and libsnappy

--- reton/Jailbreak/amfid.m
+++ reton/Jailbreak/amfid.m
@@ -8,10 +8,15 @@
 #import <Foundation/Foundation.h>
 #include "../Exploit/cicuta_virosa.h"
 
+
 int amfid_patches(uint64_t cr_label){
     uint32_t uid = getuid();
     if(uid != 0) return 0;
     printf("Trying to patch amfid slot...\n");
-    printf("amfid slot -> %llu", read_64(cr_label + 0x8));
+    printf("amfid slot found -> 0x%llx\n", read_64(cr_label + 0x8));
+    printf("patching amfid...");
+    uint32_t buffer[5] = {0, 0, 0, 1, 0};
+    write_64(cr_label + 0x8, (void*)buffer);
+    printf("\tdone (0x%llx)\n", read_64(cr_label + 0x8));
     return 0;
 }
